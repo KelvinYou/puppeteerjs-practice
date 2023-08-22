55d9f8479e4c0390670209f7bf758b2e0013c01d
Kelvin <ykekelvin0220@gmail.com>
2023-08-22 08:15:57 +0800

    backup

--- submit-google-form/index.js
+++ submit-google-form/index.js
@@ -1,9 +1,7 @@
 const puppeteer = require('puppeteer');
 
-// radio buttons
-const radioSelectBox = '#mG61Hd > div.RH5hzf.RLS9Fe > div > div.o3Dpx';
-const radioBtn1 = radioSelectBox + ' > div:nth-child(1) > div > div > div.oyXaNc > div > div > span > div > div:nth-child(1) > label > div > div.d7L4fc.bJNwt.FXLARc.aomaEc.ECvBRb';
-const radioBtn2 = radioSelectBox + ' > div:nth-child(1) > div > div > div.oyXaNc > div > div > span > div > div:nth-child(2) > label > div > div.d7L4fc.bJNwt.FXLARc.aomaEc.ECvBRb';
+const FORM_URL = "https://forms.gle/G4CXU2AAfQpGencj9";
+
 
 async function launchBrowser() {
   return puppeteer.launch({
@@ -29,6 +27,11 @@ async function waitForSelector(page, selector) {
   await page.waitForSelector(selector);
 }
 
+// radio buttons
+const radioSelectBox = '#mG61Hd > div.RH5hzf.RLS9Fe > div > div.o3Dpx';
+const radioBtn1 = radioSelectBox + ' > div:nth-child(1) > div > div > div.oyXaNc > div > div > span > div > div:nth-child(1) > label > div > div.d7L4fc.bJNwt.FXLARc.aomaEc.ECvBRb';
+const radioBtn2 = radioSelectBox + ' > div:nth-child(1) > div > div > div.oyXaNc > div > div > span > div > div:nth-child(2) > label > div > div.d7L4fc.bJNwt.FXLARc.aomaEc.ECvBRb';
+
 async function randomClickRadio(page) {
   const randomNum = Math.random();
   if (randomNum < 0.5) {
@@ -38,9 +41,32 @@ async function randomClickRadio(page) {
   }
 }
 
-(async () => {
+const textField1 = "#mG61Hd > div.RH5hzf.RLS9Fe > div > div.o3Dpx > div:nth-child(2) > div > div > div.AgroKb > div > div.RpC4Ne.oJeWuf > div.Pc9Gce.Wic03c > textarea";
+
+const checkBox1 = "#mG61Hd > div.RH5hzf.RLS9Fe > div > div.o3Dpx > div:nth-child(3) > div > div > div.Y6Myld > div:nth-child(2) > div:nth-child(1) > label > div";
+const checkBox2 = "#mG61Hd > div.RH5hzf.RLS9Fe > div > div.o3Dpx > div:nth-child(3) > div > div > div.Y6Myld > div:nth-child(2) > div:nth-child(2) > label > div";
+const checkBox3 = "#mG61Hd > div.RH5hzf.RLS9Fe > div > div.o3Dpx > div:nth-child(3) > div > div > div.Y6Myld > div:nth-child(2) > div:nth-child(3) > label > div";
+
+const checkBoxes = [checkBox1, checkBox2, checkBox3];
+
+async function randomSelectCheckboxes(page) {
+  const shuffledCheckboxes = [...checkBoxes]; // Create a copy of the checkboxes array
+  for (let i = shuffledCheckboxes.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [shuffledCheckboxes[i], shuffledCheckboxes[j]] = [shuffledCheckboxes[j], shuffledCheckboxes[i]];
+  }
+
+  const numberOfCheckboxesToSelect = Math.floor(Math.random() * 3) + 1; // Random number from 1 to 3
+
+  for (let i = 0; i < numberOfCheckboxesToSelect; i++) {
+    const checkboxSelector = shuffledCheckboxes[i];
+    await page.click(checkboxSelector);
+  }
+}
+
+main = async () => {
   const browser = await launchBrowser();
-  const page = await openPage(browser, "https://forms.gle/G4CXU2AAfQpGencj9");
+  const page = await openPage(browser, FORM_URL);
   
   await Promise.all([
     page.waitForNavigation({ waitUntil: ["domcontentloaded", "networkidle0"] }),
@@ -49,6 +75,13 @@ async function randomClickRadio(page) {
 
   await randomClickRadio(page);
 
+  await page.focus(textField1);
+  await page.keyboard.type("Steve, John, Kelvin");
+
+  await randomSelectCheckboxes(page);
+
   // Don't forget to close the browser when you're done
   // await browser.close();
-})();
+};
+
+(() => main()) ();
